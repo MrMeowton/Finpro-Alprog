c624c21496c53078b444581e43f038eb01f15f8b
sultanariq <zsultanariq@gmail.com>
2026-05-24 18:53:30 +0700

    Update LibraryManager.h

--- Managers/LibraryManager.h
+++ Managers/LibraryManager.h
@@ -1,60 +1,98 @@
 #pragma once
 
 #include <iostream>
-#include <vector>
-#include "../models/Book.h"
-#include "../structures/LinkedList.h"
-#include "../algorithms/Searching.h"
-#include "../algorithms/Sorting.h"
-#include <fstream>
-#include "../networking/json.hpp"
 
-using json = nlohmann::json;
 using namespace std;
 
-class LibraryManager {
+template <typename T>
+class Node {
+public:
+    T data;
+    Node* next;
+
+    Node(T data) {
+        this->data = data;
+        next = nullptr;
+    }
+};
+
+template <typename T>
+class LinkedList {
 private:
-    LinkedList<Book> books;
+    Node<T>* head;
 
 public:
-    // Add Book
-    void addBook(Book book) {
-        books.insert(book);
 
-        cout << endl;
-        cout << "[SUCCESS] Book added successfully!" << endl;
-
-        saveBooksToFile();
+    LinkedList() {
+        head = nullptr;
     }
 
-    // Display All Books
-    void displayBooks() {
+    // tambah data
+    void insert(T data) {
 
-    if (books.isEmpty()) {
+        Node<T>* newNode = new Node<T>(data);
 
-        cout << endl;
-        cout << "[INFO] No books available." << endl;
+        if (head == nullptr) {
+            head = newNode;
+            return;
+        }
 
-        return;
+        Node<T>* current = head;
+
+        while (current->next != nullptr) {
+            current = current->next;
+        }
+
+        current->next = newNode;
     }
 
-    Node<Book>* current = books.getHead();
+    // hapus data berdasarkan id
+    void remove(T data) {
 
-    while (current != nullptr) {
+        if (head == nullptr) {
+            return;
+        }
 
-        current->data.displayBook();
+        // kalau head yang dihapus
+        if (head->data.getId() == data.getId()) {
 
-        current = current->next;
+            Node<T>* temp = head;
+
+            head = head->next;
+
+            delete temp;
+
+            return;
+        }
+
+        Node<T>* current = head;
+
+        while (current->next != nullptr) {
+
+            if (current->next->data.getId() == data.getId()) {
+
+                Node<T>* temp = current->next;
+
+                current->next = current->next->next;
+
+                delete temp;
+
+                return;
+            }
+
+            current = current->next;
+        }
     }
-    }
 
-    // Search Book By ID
-    Book* searchBookById(int id) {
-        Node<Book>* current = books.getHead();
+    // cari data
+    Node<T>* find(int id) {
+
+        Node<T>* current = head;
 
         while (current != nullptr) {
+
             if (current->data.getId() == id) {
-                return &(current->data);
+                return current;
             }
 
             current = current->next;
@@ -63,191 +101,26 @@ public:
         return nullptr;
     }
 
-    // Borrow Book
-    bool borrowBook(int id) {
-        Book* book = searchBookById(id);
+    // tampilkan semua data
+    void traversal() {
 
-        if (book == nullptr) {
-            cout << endl;
-            cout << "[ERROR] Book not found!" << endl;
-            return false;
+        Node<T>* current = head;
+
+        while (current != nullptr) {
+
+            current->data.displayBook();
+
+            current = current->next;
         }
-
-        if (book->getStock() <= 0) {
-            cout << endl;
-            cout << "[ERROR] Book out of stock!" << endl;
-            return false;
-        }
-
-        book->setStock(book->getStock() - 1);
-
-        cout << endl;
-        cout << "[SUCCESS] Book borrowed successfully!" << endl;
-
-        saveBooksToFile();
-        return true;
     }
 
-    // Return Book
-    void returnBook(int id) {
-        Book* book = searchBookById(id);
-
-        if (book == nullptr) {
-            cout << endl;
-            cout << "[ERROR] Book not found!" << endl;
-            return;
-        }
-
-        book->setStock(book->getStock() + 1);
-
-        cout << endl;
-        cout << "[SUCCESS] Book returned successfully!" << endl;
+    // cek list kosong
+    bool isEmpty() {
+        return head == nullptr;
     }
 
-    // TAMBAHAN PHASE 3
-    // Convert LinkedList → Vector
-    vector<Book> convertToVector() {
-
-    vector<Book> tempBooks;
-
-    Node<Book>* current = books.getHead();
-
-    while (current != nullptr) {
-
-        tempBooks.push_back(current->data);
-
-        current = current->next;
-    }
-
-    return tempBooks;
-    }
-
-    //SEARCH TITLE
-    void searchBookByTitle(string title) {
-    vector<Book> tempBooks =
-        convertToVector();
-
-    Book* result =
-        Searching::linearSearchByTitle(
-            tempBooks,
-            title
-        );
-
-    if (result == nullptr) {
-
-        cout << endl;
-        cout << "[INFO] Book not found!" << endl;
-
-        return;
-    }
-
-    cout << endl;
-    cout << "===== SEARCH RESULT =====" << endl;
-
-    result->displayBook();
-    }
-
-    //SORT TITLE
-    void sortBooksByTitle() {
-
-    vector<Book> tempBooks =
-        convertToVector();
-
-    Sorting::quickSortByTitle(
-        tempBooks,
-        0,
-        tempBooks.size() - 1
-    );
-
-    cout << endl;
-    cout << "===== SORTED BY TITLE =====" << endl;
-
-    for (Book& book : tempBooks) {
-        book.displayBook();
-    }
-    }
-
-    // SORT STOCK
-    void sortBooksByStock() {
-    vector<Book> tempBooks =
-        convertToVector();
-
-    Sorting::mergeSortByStock(
-        tempBooks,
-        0,
-        tempBooks.size() - 1
-    );
-
-    cout << endl;
-    cout << "===== SORTED BY STOCK =====" << endl;
-
-    for (Book& book : tempBooks) {
-        book.displayBook();
-    }
-    }
-
-    void loadBooksFromFile() {
-
-    ifstream file("data/books.json");
-
-    if (!file.is_open()) {
-
-        cout << "[ERROR] Cannot open books.json!" << endl;
-
-        return;
-    }
-
-    json data;
-
-    file >> data;
-
-    for (auto& item : data) {
-
-        Book book(
-            item["id"],
-            item["title"],
-            item["author"],
-            item["category"],
-            item["stock"]
-        );
-
-        books.insert(book);
-    }
-
-    file.close();
-
-    cout << endl;
-    cout << "[SUCCESS] Books loaded from JSON!" << endl;
-    }
-
-    void saveBooksToFile() {
-
-    json data = json::array();
-
-    Node<Book>* current = books.getHead();
-
-    while (current != nullptr) {
-
-        json item;
-
-        item["id"] = current->data.getId();
-        item["title"] = current->data.getTitle();
-        item["author"] = current->data.getAuthor();
-        item["category"] = current->data.getCategory();
-        item["stock"] = current->data.getStock();
-
-        data.push_back(item);
-
-        current = current->next;
-    }
-
-    ofstream file("data/books.json");
-
-    file << data.dump(4);
-
-    file.close();
-
-    cout << endl;
-    cout << "[SUCCESS] Books saved to JSON!" << endl;
+    // ambil head
+    Node<T>* getHead() {
+        return head;
     }
 };
