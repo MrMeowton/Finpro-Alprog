7e1489e6576cae760c695ede24cea605dc0c1ce0
enderzw <enderzw@gmail.com>
2026-05-25 19:12:48 +0700

    Update Searching.h

--- Algorithms/Searching.h
+++ Algorithms/Searching.h
@@ -11,88 +11,40 @@ class Searching {
 public:
 
     // mencari berdasarkan judul dengan linear search
-
     static Book* linearSearchByTitle(
         vector<Book>& books,
         string keyword
     ) {
-
         for (Book& book : books) {
-
             if (book.getTitle() == keyword) {
                 return &book;
             }
         }
-
         return nullptr;
     }
 
     // mencari berdasarkan author dengan linear search
-
     static Book* linearSearchByAuthor(
         vector<Book>& books,
         string keyword
     ) {
-
         for (Book& book : books) {
-
             if (book.getAuthor() == keyword) {
                 return &book;
             }
         }
-
         return nullptr;
     }
 
-    // mengecek apakah data sudah terurut berdasarkan ID
-
-    static bool isSortedById(
-        vector<Book>& books
-    ) {
-
-        for (int i = 0; i < books.size() - 1; i++) {
-
-            if (books[i].getId() > books[i + 1].getId()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
-    // mengecek apakah data sudah terurut berdasarkan title
-
-    static bool isSortedByTitle(
-        vector<Book>& books
-    ) {
-
-        for (int i = 0; i < books.size() - 1; i++) {
-
-            if (books[i].getTitle() > books[i + 1].getTitle()) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-
     // mencari berdasarkan ID dengan binary search
-
     static Book* binarySearchById(
         vector<Book>& books,
         int targetId
     ) {
-
-        // cek apakah data sudah sorted
-        if (!isSortedById(books)) {
-            return nullptr;
-        }
-
         int left = 0;
         int right = books.size() - 1;
 
         while (left <= right) {
-
             int mid = left + (right - left) / 2;
 
             if (books[mid].getId() == targetId) {
@@ -110,24 +62,16 @@ public:
         return nullptr;
     }
 
-    // mencari berdasarkan title dengan binary search
-
+    // mencari berdasarkan title dengan binary search 
     static Book* binarySearchByTitle(
         vector<Book>& books,
         string target
     ) {
-
-        // cek apakah data sudah sorted
-        if (!isSortedByTitle(books)) {
-            return nullptr;
-        }
-
         int left = 0;
         int right = books.size() - 1;
 
         while (left <= right) {
-
-            int mid = left + (right - left) / 2;
+            int mid = left + (right - left) / 2; 
 
             if (books[mid].getTitle() == target) {
                 return &books[mid];
