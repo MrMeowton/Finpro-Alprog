63cc94819324e42067d43d650bf1e54ef2bad562
enderzw <enderzw@gmail.com>
2026-05-25 19:18:35 +0700

    Update LinkedList.h

--- Structures/LinkedList.h
+++ Structures/LinkedList.h
@@ -20,59 +20,83 @@ struct Node {
 template <typename T>
 class LinkedList {
 private:
+
     Node<T>* head;
 
 public:
 
     // Constructor
     LinkedList() {
+
         head = nullptr;
     }
 
     // Destructor
-
     ~LinkedList() {
 
         Node<T>* current = head;
 
         while (current != nullptr) {
+
             Node<T>* temp = current;
+
             current = current->next;
+
             delete temp;
         }
     }
 
-    // menambahkan node baru ke akhir linked list
-
+    // Insert Node
     void insert(T data) {
 
-        Node<T>* newNode = new Node<T>(data);
+        Node<T>* newNode =
+            new Node<T>(data);
 
+        // jika linked list kosong
         if (head == nullptr) {
+
             head = newNode;
+
             return;
         }
 
         Node<T>* current = head;
 
+        // menuju node terakhir
         while (current->next != nullptr) {
             current = current->next;
         }
+
+        // sambungkan node baru
         current->next = newNode;
     }
 
-    // transversal linked list
-    void display() const {
+    // Get Head
+    Node<T>* getHead() const {
+
+        return head;
+    }
+
+    // Check Empty
+    bool isEmpty() const {
+
+        return head == nullptr;
+    }
+
+    // Count Size
+    int size() const {
+
+        int count = 0;
 
         Node<T>* current = head;
 
         while (current != nullptr) {
 
-            cout << current->data << " -> ";
+            count++;
 
             current = current->next;
         }
 
-        cout << "nullptr" << endl;
+        return count;
     }
 };
